4eef3781d656d2c12bd6399e61523d8e5d97c27f
yanbo <10955940+yanbostudio@users.noreply.github.com>
2022-05-13 11:41:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
-import base64
-
+import os
 import requests
+
 from requests.auth import HTTPBasicAuth
 
-WEBHOOK_URL = "https://xxxxxxxxxxxxx"
-PROM_USER = "test_user"
-PROM_PASS = "test_pass"
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+PROM_USER = os.getenv("PROM_USER")
+PROM_PASS = os.getenv("PROM_PASS")
+
 AUTH = HTTPBasicAuth(PROM_USER, PROM_PASS)
 
 
-def hello_pubsub(event, context):
+def triggered_pubsub(event, context):
     """Triggered from a message on a Cloud Pub/Sub topic.
     Args:
          event (dict): Event payload.
          context (google.cloud.functions.Context): Metadata for the event.
     """
-    content = base64.b64decode(event['data']).decode('utf-8')
 
-    payload_message = {
-        "msg_type": "text",
-        "content": content
-    }
+    headers = {'Content-Type': 'application/json'}
+    payload_message = event
+
+    resp = requests.post(WEBHOOK_URL, json=payload_message, headers=headers, auth=AUTH)
+
+    return f'ok'
+
+
+def triggered_http(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+
+    payload_message = request.get_json()
 
     headers = {
         'Content-Type': 'application/json'
@@ -29,4 +44,3 @@ def hello_pubsub(event, context):
     resp = requests.post(WEBHOOK_URL, json=payload_message, headers=headers, auth=AUTH)
 
     return f'ok'
-
